ae7beeb196cd9e8bafac3105073e539c0b22fb3a
Igor Vylushchak <69826970+linker-din@users.noreply.github.com>
2023-05-28 18:20:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,59 +4,54 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Make requests initially and every 3 seconds
     setInterval(function() {
-        symbols.forEach(function(symbol) {
-            makeRequest(symbol);
-        });
+        makeRequest();
         displayRefreshMessage();
     }, 3000);
 
-    function makeRequest(symbol) {
+    function makeRequest() {
         // Generate a random number to use as a cache-busting parameter
         var cacheBuster = Math.random();
 
-        // Make a request to the API endpoint
-        var request = new XMLHttpRequest();
-        var url = "https://arcane-shelf-63340.herokuapp.com/api/v1/market/orderbook/level1";
-        var params = { "symbol": symbol, "cacheBuster": cacheBuster };
-        var paramString = Object.keys(params).map(function(key) {
-            return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
-        }).join("&");
+        symbols.forEach(function(symbol) {
+            // Make a request to the API endpoint
+            var request = new XMLHttpRequest();
+            var url = "https://arcane-shelf-63340.herokuapp.com/api/v1/market/orderbook/level1";
+            var params = { "symbol": symbol, "cacheBuster": cacheBuster };
+            var paramString = Object.keys(params).map(function(key) {
+                return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
+            }).join("&");
 
-        request.open('GET', url + "?" + paramString, true);
+            request.open('GET', url + "?" + paramString, true);
 
-        request.onload = function() {
-            if (request.status >= 200 && request.status < 400) {
-                // Parse the JSON response
-                var response = JSON.parse(request.responseText);
-                var price = response.price;
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    // Parse the JSON response
+                    var response = JSON.parse(request.responseText);
+                    var price = response.price;
 
-                // Create a div element to display the coin price
-                var coinDiv = document.createElement('div');
-                coinDiv.textContent = symbol + ": " + price;
-                coinDiv.classList.add("coinPrice");
+                    // Create a div element to display the coin price
+                    var coinDiv = document.createElement('div');
+                    coinDiv.textContent = symbol + ": " + price;
+                    coinDiv.classList.add("coinPrice");
 
-                // Append the div to the coinPrices element
-                var coinPricesDiv = document.getElementById('coinPrices');
-                if (coinPricesDiv) {
-                    // Clear previous coin prices
-                    while (coinPricesDiv.firstChild) {
-                        coinPricesDiv.removeChild(coinPricesDiv.firstChild);
+                    // Append the div to the coinPrices element
+                    var coinPricesDiv = document.getElementById('coinPrices');
+                    if (coinPricesDiv) {
+                        coinPricesDiv.appendChild(coinDiv);
+                    } else {
+                        console.error('coinPrices element not found');
                     }
-
-                    coinPricesDiv.appendChild(coinDiv);
                 } else {
-                    console.error('coinPrices element not found');
+                    console.error('Error: ' + request.status);
                 }
-            } else {
-                console.error('Error: ' + request.status);
-            }
-        };
+            };
 
-        request.onerror = function() {
-            console.error('Request failed');
-        };
+            request.onerror = function() {
+                console.error('Request failed');
+            };
 
-        request.send();
+            request.send();
+        });
     }
 
     function displayRefreshMessage() {
