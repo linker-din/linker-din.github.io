027ce80d03c78da13c217e2f0d326b6ae39df7f0
Igor Vylushchak <69826970+linker-din@users.noreply.github.com>
2023-05-28 17:21:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const symbols = ['LMWR-USDT', 'KCS-USDT'];
-  const tokenPricesContainer = document.getElementById('tokenPrices');
-
-  symbols.forEach(async (symbol) => {
-    const apiUrl = 'https://arcane-shelf-63340.herokuapp.com/api/v1/market/orderbook/level1';
-    const params = { symbol };
-
-    try {
-      const response = await fetch(apiUrl, { params });
-      if (!response.ok) {
-        throw new Error('Request failed');
+// API request function
+function fetchTokenPrice(url) {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data && data.data && data.data.price) {
+          return data.data.price;
+        } else {
+          throw new Error('Invalid response data');
+        }
+      })
+      .catch(error => {
+        console.log('Error:', error);
+        return 'N/A';
+      });
+  }
+  
+  // Display token price
+  function displayTokenPrice(symbol, price) {
+    const tokenPriceElement = document.getElementById('tokenPrice');
+    const tokenItem = document.createElement('li');
+    tokenItem.textContent = `Symbol: ${symbol} | Price: ${price}`;
+    tokenPriceElement.appendChild(tokenItem);
+  }
+  
+  // Fetch token prices and display them
+  function fetchAndDisplayTokenPrices() {
+    const tokenList = [
+      { symbol: 'LMWR-USDT', url: 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=LMWR-USDT' },
+      { symbol: 'KCS-USDT', url: 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=KCS-USDT' }
+      // Add more tokens here as needed
+    ];
+  
+    const tokenPriceElement = document.getElementById('tokenPrice');
+    tokenPriceElement.innerHTML = ''; // Clear previous token prices
+  
+    // Fetch and display token prices sequentially
+    function fetchSequentially(index) {
+      if (index >= tokenList.length) {
+        return; // Exit when all tokens have been fetched
       }
-      const data = await response.json();
-      const price = data.price;
-
-      const tokenElement = document.createElement('div');
-      tokenElement.classList.add('token');
-
-      const symbolElement = document.createElement('span');
-      symbolElement.classList.add('symbol');
-      symbolElement.textContent = symbol;
-
-      const priceElement = document.createElement('span');
-      priceElement.classList.add('price');
-      priceElement.textContent = price;
-
-      tokenElement.appendChild(symbolElement);
-      tokenElement.appendChild(document.createTextNode(': '));
-      tokenElement.appendChild(priceElement);
-
-      tokenPricesContainer.appendChild(tokenElement);
-    } catch (error) {
-      console.error(`Error fetching data for symbol ${symbol}: ${error.message}`);
+  
+      const { symbol, url } = tokenList[index];
+      fetchTokenPrice(url)
+        .then(price => {
+          displayTokenPrice(symbol, price);
+          fetchSequentially(index + 1); // Fetch next token
+        });
     }
-  });
-});
+  
+    fetchSequentially(0); // Start fetching from the first token
+  }
+  
+  // Fetch and display token prices when the page loads
+  window.addEventListener('load', fetchAndDisplayTokenPrices);
+  
