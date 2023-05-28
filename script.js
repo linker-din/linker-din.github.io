eebe0a35c6115fd12065eac17f651e86cf87cd82
Igor Vylushchak <69826970+linker-din@users.noreply.github.com>
2023-05-28 16:27:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-const symbols = ['LMWR-USDT', 'KCS-USDT'];
-const tokenPricesContainer = document.getElementById('tokenPrices');
+document.addEventListener('DOMContentLoaded', () => {
+  const symbols = ['LMWR-USDT', 'KCS-USDT'];
+  const tokenPricesContainer = document.getElementById('tokenPrices');
 
-symbols.forEach(async (symbol) => {
-  const apiUrl = `https://arcane-shelf-63340.herokuapp.com/api/v1/market/orderbook/level1?symbol=${symbol}`;
+  symbols.forEach(async (symbol) => {
+    const apiUrl = 'https://arcane-shelf-63340.herokuapp.com/api/v1/market/orderbook/level1';
+    const params = { symbol };
 
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Request failed');
+    try {
+      const response = await fetch(apiUrl, { params });
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+      const data = await response.json();
+      const price = data.price;
+
+      const tokenElement = document.createElement('div');
+      tokenElement.classList.add('token');
+
+      const symbolElement = document.createElement('span');
+      symbolElement.classList.add('symbol');
+      symbolElement.textContent = symbol;
+
+      const priceElement = document.createElement('span');
+      priceElement.classList.add('price');
+      priceElement.textContent = price;
+
+      tokenElement.appendChild(symbolElement);
+      tokenElement.appendChild(document.createTextNode(': '));
+      tokenElement.appendChild(priceElement);
+
+      tokenPricesContainer.appendChild(tokenElement);
+    } catch (error) {
+      console.error(`Error fetching data for symbol ${symbol}: ${error.message}`);
     }
-    const data = await response.json();
-    const price = data.price;
-
-    const tokenElement = document.createElement('div');
-    tokenElement.classList.add('token');
-
-    const symbolElement = document.createElement('span');
-    symbolElement.classList.add('symbol');
-    symbolElement.textContent = symbol;
-
-    const priceElement = document.createElement('span');
-    priceElement.classList.add('price');
-    priceElement.textContent = price;
-
-    tokenElement.appendChild(symbolElement);
-    tokenElement.appendChild(document.createTextNode(': '));
-    tokenElement.appendChild(priceElement);
-
-    tokenPricesContainer.appendChild(tokenElement);
-  } catch (error) {
-    console.error(`Error fetching data for symbol ${symbol}: ${error.message}`);
-  }
+  });
 });
