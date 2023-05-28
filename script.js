7f96c15de19bc5fd353f40838344509d5c8e9d68
Igor Vylushchak <69826970+linker-din@users.noreply.github.com>
2023-05-28 16:24:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const symbols = ['LMWR-USDT', 'KCS-USDT'];
+const tokenPricesContainer = document.getElementById('tokenPrices');
 
 symbols.forEach(async (symbol) => {
-  const apiUrl = `https://git.heroku.com/arcane-shelf-63340.git/api/v1/market/orderbook/level1?symbol=${symbol}`;
+  const apiUrl = `https://arcane-shelf-63340.herokuapp.com/api/v1/market/orderbook/level1?symbol=${symbol}`;
 
   try {
     const response = await fetch(apiUrl);
@@ -10,9 +11,24 @@ symbols.forEach(async (symbol) => {
     }
     const data = await response.json();
     const price = data.price;
-    console.log(`Symbol: ${symbol}, Price: ${price}`);
+
+    const tokenElement = document.createElement('div');
+    tokenElement.classList.add('token');
+
+    const symbolElement = document.createElement('span');
+    symbolElement.classList.add('symbol');
+    symbolElement.textContent = symbol;
+
+    const priceElement = document.createElement('span');
+    priceElement.classList.add('price');
+    priceElement.textContent = price;
+
+    tokenElement.appendChild(symbolElement);
+    tokenElement.appendChild(document.createTextNode(': '));
+    tokenElement.appendChild(priceElement);
+
+    tokenPricesContainer.appendChild(tokenElement);
   } catch (error) {
     console.error(`Error fetching data for symbol ${symbol}: ${error.message}`);
   }
 });
-
